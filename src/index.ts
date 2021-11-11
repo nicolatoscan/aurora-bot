a78dd34afed576bf1480095a4a31a7a90dd100aa
Nicola Toscan <nicolatoscan99@outlook.it>
2021-11-11 02:55:20 +0100

    forecast

--- src/index.ts
+++ src/index.ts
@@ -4,10 +4,11 @@ import { Telegraf, Context } from "telegraf";
 dotenv.config()
 
 interface KpData {
-    time: string;
+    time: Date;
     kp: number;
-    kpFrac: number;
 }
+
+enum DateFormat { DATE, TIME };
 
 class AuroraBot {
     private bot: Telegraf<Context>;
@@ -22,38 +23,62 @@ class AuroraBot {
         this.bot = new Telegraf(TOKEN);
         this.bot.start((ctx: Context) => ctx.reply('Welcome!'));
         this.bot.command('k', (ctx) => this.lastK(ctx));
+        this.bot.command('forecast', (ctx) => this.forecast(ctx));
         this.bot.command('history', (ctx) => this.history(ctx));
 
         this.bot.launch();
         console.log('Bot started');
     }
 
-    private async getKpData(): Promise<KpData[]> {
-        const res = await axios.get('http://services.swpc.noaa.gov/products/noaa-planetary-k-index.json');
+    private async getKpData(url: string): Promise<KpData[]> {
+        const res = await axios.get(url);
         return res.data.slice(1).map((x: any) => ({
-            time: x[0].substring(5, 13),
-            kp: +x[1],
-            kpFrac: +x[2]
+            time: new Date(x[0]),
+            kp: +x[1]
         }))
     }
 
-    private getLine(d: KpData): string {
-        const alert = d.kp >= 7 ? '🔴' : (d.kp >= 5 ? '🟡' : '🟢');
-        return `${alert} ${d.time} - ${d.kp} (${d.kpFrac})`
+    private async getPastKp(): Promise<KpData[]> {
+        return this.getKpData('http://services.swpc.noaa.gov/products/noaa-planetary-k-index.json');
     }
 
+    private async getMinuteKp(): Promise<KpData[]> {
+        return this.getKpData('http://services.swpc.noaa.gov/products/noaa-estimated-planetary-k-index-1-minute.json');
+    }
+
+    private async getForecastKp(): Promise<KpData[]> {
+        return this.getKpData('http://services.swpc.noaa.gov/products/noaa-planetary-k-index-forecast.json');
+    }
+
+    private getLine(d: KpData, minutes: boolean = false): string {
+        const roundedKP = Math.min(8, Math.round(d.kp));
+        // const alert = '🟢🟢🟢🔵🟣🟡🟠🔴🔴'[roundedKP];
+        const alert = ['🟢', '🟢', '🟢', '🔵', '🟣', '🟡', '🟠', '🔴', '🔴'][roundedKP];
+        const bar = ''.padEnd(roundedKP, '█') + ''.padEnd(8 - roundedKP, '░');
+        const date = minutes 
+            ? ` ${d.time.toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'})}`
+            : `${d.time.getDate().toString().padStart(2, '0')} ${d.time.toLocaleString('default', { month: 'short' })} - ${d.time.getHours().toString().padStart(2, '0')}h`;
+        return `${alert}${d.kp.toFixed(1)} ${bar} ${date} `
+    }
+
+    private getLines(datas: KpData[], fullDate: boolean = false): string {
+        return datas.map(x => this.getLine(x, fullDate)).join('\n');
+    }
 
     private async lastK(ctx: Context) {
-        const kpData = (await this.getKpData()).slice(-1)[0];
-        ctx.reply(`Current Kp index:\n${this.getLine(kpData)}`);
+        const kpData = (await this.getMinuteKp()).slice(-50).filter((x, i) => i % 5 === 0);
+        ctx.reply(`Last recorded Kp indexes:\n${this.getLines(kpData, true)}`);
+    }
+
+    private async forecast(ctx: Context) {
+        const kpData = (await this.getForecastKp()).filter(x => x.time >= new Date());
+        ctx.reply(`Forecasts:\n${this.getLines(kpData)}`);
     }
 
     private async history(ctx: Context) {
-        const kpData = (await this.getKpData()).slice(-10);
-        const response = `Last 10 Kp indexes:\n${kpData.map(x => this.getLine(x)).join('\n')}`;
-        ctx.reply(response);
+        const kpData = (await this.getPastKp()).slice(-10);
+        ctx.reply(`Last recorded Kp indexes:\n${this.getLines(kpData)}`);
     }
-
 }
 
 new AuroraBot();
